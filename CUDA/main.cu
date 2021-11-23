b0b92990dc907c91b57651caf2d1b0be4e28e270
robvold <robvold98@gmail.com>
2021-11-23 18:15:49 +0100

    Fiksa flere feil med koden. Nå vil ojas rule fungere med flere paches/segmenter. Verdier ser ut til å gå mot null ved høyt antall segmenter inn

--- CUDA/main.cu
+++ CUDA/main.cu
@@ -8,97 +8,65 @@
 using namespace std;
 
 __global__ void w_ojas(float *x, float *w, const float y, const float learning_rate) {
-    size_t i = blockIdx.x * blockDim.x + threadIdx.x;
+    size_t i = threadIdx.x;
     float temp = x[i] - y * w[i];
     w[i] = w[i] + learning_rate * y * temp;
 };
 
-__device__ float y_ojas(const float *w, const float *x) {
-    const int length = 25;
+__device__ float y_ojas(const float *w, const float *x, const int len) {
     float y = 0;
-    for (int i = 0; i < length; ++i) {
+    for (int i = 0; i < len; ++i) {
         y += w[i] * x[i];
     }
     return y;
 }
 
-__global__ void ojas_rule(float *x, float *w, const float learning_rate, const int num, const int seg_len) {
+__global__ void ojas_rule(float *x, float *w, const float learning_rate, const int num, const int len) {
     float y;
 
-    printf("test\n");
-
     for (int i = 0; i < num; ++i) {
-        //printf("const char *, ...");
-        //printf("%f", x[i]);
-        //Må sende inn riktig deler av x
-        //y = y_ojas(w, x);
-        //w_ojas<<<1, 25>>>(x, w, y, learning_rate);
-        //cudaDeviceSynchronize();
+        float *x_start = &(x[i * len]); //Må sende inn riktig deler av x
+        y = y_ojas(w, x_start, len);
+
+        w_ojas<<<1, 25>>>(x_start, w, y, learning_rate);
+        cudaDeviceSynchronize();
     }
 }
 
-__host__ void run_ojas(float *w, vector<vector<float>> vec_x, const int num, const int seg_len) {
+__host__ void run_ojas(float *w, vector<float> vec_x, const int num, const int len) {
 
-    float x[num][seg_len];
-    /*for (int i = 0; i < num; ++i) {
-        for (int j = 0; j < seg_len; ++j) {
-            x[i][j] = (*vec_x.data())[j];
-            printf("%f", (*vec_x.data())[j]);
-        };
-    }*/
-    cout << endl;
-    cout << endl;
-    int a = 0;
-    int b = 0;
-    for (auto i : vec_x) {
-        for (auto j : i) {
-            x[a][b] = j;
-            b++;
-        }
-        a++;
-    }
-
-    cout << endl;
-    cout << endl;
-    cout << endl;
-
-    for (int i = 0; i < num; ++i) {
-        for (int j = 0; j < seg_len; ++j) {
-            printf("%f", x[i][j]);
-        };
-    }
-
-    cout << endl;
-    cout << endl;
-
-    const float learning_rate = 0.1;
+    float *x = vec_x.data();
     float *d_w, *d_x;
+    const float learning_rate = 0.1;
+    const size_t x_size = sizeof(*x) * num * len;
+    const size_t w_size = sizeof(*w) * len;
 
-    cudaMalloc(&d_w, sizeof(w));
-    cudaMalloc(&d_x, sizeof(x));
+    cudaMalloc(&d_w, w_size);
+    cudaMalloc(&d_x, x_size);
 
-    cudaMemcpy(d_w, w, sizeof(w), cudaMemcpyHostToDevice);
-    cudaMemcpy(d_x, x, sizeof(x), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_w, w, w_size, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_x, x, x_size, cudaMemcpyHostToDevice);
 
-    ojas_rule<<<1, 1>>>(d_x, d_w, learning_rate, num, seg_len);
+    ojas_rule<<<1, 1>>>(d_x, d_w, learning_rate, num, len);
 
     cudaDeviceSynchronize();
-    cudaMemcpy(w, d_w, sizeof(w), cudaMemcpyDeviceToHost);
+    cudaMemcpy(w, d_w, w_size, cudaMemcpyDeviceToHost);
 
     cudaFree(d_w);
     cudaFree(d_x);
 }
 
-__host__ vector<vector<float>> load_data(const int num) {
+__host__ vector<float> load_data(const int num) {
     mnist_loader train("datasets/train-images.idx3-ubyte",
                        "datasets/train-labels.idx1-ubyte", 100);
     mnist_loader test("datasets/t10k-images.idx3-ubyte",
                       "datasets/t10k-labels.idx1-ubyte", 100);
 
-    vector<vector<float>> segments;
+    vector<float> segments;
 
     for (int i = 0; i < num; ++i) {
-        segments.emplace_back(train.image_segment());
+        vector<float> segment = train.image_segment();
+        segments.insert(segments.end(), segment.begin(), segment.end());
     }
 
     return segments;
@@ -118,7 +86,7 @@ __host__ float *generate_w(const int len) {
 //må free w og x;
 int main() {
 
-    const int num_seg = 10;     //ant segmenter
+    const int num_seg = 100000; //ant segmenter
     const int len = 25;         //lengden på et segment
     float *w = generate_w(len); // skal bare være 25 lang
 
@@ -126,13 +94,14 @@ int main() {
     for (int j = 0; j < len; ++j) {
         cout << w[j] << ", ";
     }
-    cout << endl;
 
-    vector<vector<float>> x = load_data(num_seg);
+    vector<float> x = load_data(num_seg);
 
     run_ojas(w, x, num_seg, len);
 
-    cout << "W(1):" << endl;
+    cout << endl;
+    cout << endl;
+    cout << "w(" << num_seg << "):" << endl;
     for (int i = 0; i < 25; ++i) {
         cout << w[i] << ", ";
     }
