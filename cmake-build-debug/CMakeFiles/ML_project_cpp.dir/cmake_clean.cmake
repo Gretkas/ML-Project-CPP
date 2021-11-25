5be442eeda263aca9ada5e087d5d3bbaed6bce36
Sergio Martinez <sergimar@stud.ntnu.no>
2021-11-25 12:26:03 +0100

    added build

---
+++ cmake-build-debug/CMakeFiles/ML_project_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ML_project_cpp.dir/src/ClHelper.cpp.o"
+  "CMakeFiles/ML_project_cpp.dir/src/MNIST/mnist_loader.cpp.o"
+  "CMakeFiles/ML_project_cpp.dir/src/Model.cpp.o"
+  "CMakeFiles/ML_project_cpp.dir/src/main.cpp.o"
+  "ML_project_cpp"
+  "ML_project_cpp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ML_project_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
